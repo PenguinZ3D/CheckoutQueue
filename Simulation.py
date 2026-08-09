fb33d829965de1f49dd6f6c52b3c5265aeea9b28
Rushil Patel <109630001+PenguinZ3D@users.noreply.github.com>
2026-08-09 15:41:05 -0400

    Simulation Loop and Customer Arrival
    Simulation time loop created
    Customers are added after a randomized amount of time
    Queue length is changed when customer arrives

--- Simulation.py
+++ Simulation.py
@@ -1,10 +1,34 @@
 from customer import Customer
+import random
 
 #Simulation parameters
-current_time = 0
+current_time = 0.0
+
+simulation_duration = 10.0
+
+working_time = 0
 
 queue = []
 
 cashier_busy = False
 
 customers_served = 0
+
+next_arrival_time = random.uniform(1, 4)
+
+
+
+
+while current_time < simulation_duration:
+    current_time += 1
+    print("Current Time: " + str(current_time)) #To test time increment
+
+    if current_time >= next_arrival_time:
+        customer = Customer(current_time, random.uniform(2, 5))
+        queue.append(customer)
+        next_arrival_time = current_time + random.uniform(1, 4)
+        print("Customer Arrived at: " + str(customer.arrival_time)) #To test arrival
+
+        print(len(queue)) #To test queue length
+        
+
